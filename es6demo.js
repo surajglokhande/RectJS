21ef8b059b3bd3c0482c55a592d49a2414f6fa0c
Suraj Lokhande <surajglokhande@gmail.com>
2020-10-06 15:39:11 +0530

    new feature is added

--- es6demo.js
+++ es6demo.js
@@ -1,5 +1,5 @@
 /*array destructuring*/
-
+/*
 var num = [5,2,3,4]
 let [a,b, ...c] = num
 console.log(a,b,c)
@@ -9,14 +9,18 @@ function arrayNum() {
 }
 [x=3, y=4] = arrayNum()
 console.log(x,y)
+*/
 
 /*object destructuring*/
+/*
 let person = { fname: "Suraj", lname: "Lokhande", city:"pune" }
 
 let { fname: fn, lname: ln, city } = person //undefined "Lokhande" "pune"
 console.log(fn, ln, city)
+*/
 
 /* templte literals */
+/*
 let temp = `
     sgcackjvl.kvre
     sdjvkhvk.ahvkav\
@@ -27,10 +31,10 @@ console.log(temp)
 
 let name = "suraj lokhande"
 console.log(`name is: ${name}`)
-
+*/
 
 /* function calling */
-
+/*
 let someFunc = Function()
 someFunc()
 
@@ -41,4 +45,64 @@ secoundFunc(5)
 
 let thirdFunc = x => console.log("single parameter", x)
 thirdFunc(10)
+*/
+
+/* Promise */
+/*var flag = false
+//create Promises
+let myNewPromise = new Promise((resolve, reject) => {
+    if (flag) {
+        resolve("I did Somthing")
+    }else{
+        reject("somthing did wrong")
+    }
+})
+
+//consume promises
+myNewPromise
+.then((result)=> console.log(result))
+.catch((error) => console.log(error))  */           
+
+
+// fake API calling       
+/*async function myAgainPromises() {
+    let response = await fetch('http://dummy.restapiexample.com/api/v1/employees')
+    let dataRespo = await response.json()
+    return dataRespo
+}
+
+let data = myAgainPromises()
+console.log(data.then((response)=>(console.log(response))))
+
+// //handling multiple promises
+let p1 = new Promise((resolve)=> resolve("promise 1"))
+let p2 = new Promise((resolve,reject)=> reject("promise 2 rejected"))
+let p3 = new Promise((resolve)=> resolve("promise 3"))
+Promise.all([p1,p2,p3])
+.then((result)=>console.log(result))
+.catch((err)=>console.log(err))*/
+
+/* class & getter and setter */
+/*class Employee{
+    constructor(name){
+        this.fullname = name
+    }
+    set changeName(name){
+        this.fullname = name
+    }
+    get changeName() { 
+        return this.fullname
+    }
+}
+
+let emp = new Employee("Suraj")
+console.log(emp.fullname);
+emp.changeName = "Dhiraj"
+console.log(emp.changeName);*/
+
+
+
+
+
+
 
