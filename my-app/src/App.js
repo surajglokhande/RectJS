9129030fc565898abba3551e0092ee8e6f501104
Suraj Lokhande <surajglokhande@gmail.com>
2020-10-07 16:32:09 +0530

    first rect app is created and run

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './HelloWorld';
 
 function App() {
+  let msg = "Hello, World"
+  let style1 = { color: 'red' }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>My first Application</h2>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 style = {style1} >My first Application</h2>
+        <h3 className = 'para'>{msg}</h3>
+        </header>
+      </div>     
+  );
+}
+
+function App1() {
+  return(
+    <div>
+      <Hello />
     </div>
   );
 }
 
-export default App;
+export default App1
---
+++ my-app/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello World Component</h3>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld
